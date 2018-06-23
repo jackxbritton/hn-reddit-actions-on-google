df3a6a1c82c1f273168a4e76ae9f6218934dd400
Jack <jack@medsender.com>
2018-06-23 16:26:03 -0400

    Reddit.

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const app = dialogflow();
 
 app.intent('hn-top', conv => {
 
-    // https://hacker-news.firebaseio.com/v0/topstories.json
-
+    // Fetch list of top story ids.
     return fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then((res) => res.json())
     .then((ids) => {
 
-        // Define array of request promises.
+        // Define array of request promises to get the full stories.
         let promises = [];
         for (let i = 0; i < 10; i++) {
             promises.push(fetch(`https://hacker-news.firebaseio.com/v0/item/${ids[i]}.json`)
@@ -41,6 +40,35 @@ app.intent('hn-top', conv => {
 
     }).catch((err) => {
         conv.ask('Something went wrong!');
+        console.log(err);
+    });
+
+});
+
+app.intent('reddit-top', (conv, data) => {
+
+    // Make a request for the top posts of the subreddit.
+    return fetch(`https://www.reddit.com/r/${data.subreddit}/top.json`)
+    .then((res) => res.json())
+    .then(posts => {
+
+        // The posts are actually here in the response.
+        posts = posts.data.children.splice(0, 10);
+
+        // Reply (with a browse carousel).
+        conv.ask(`Here are the top posts on /r/${data.subreddit} right now.`);
+        conv.ask(new BrowseCarousel({
+            items: posts.map(post => {
+                return new BrowseCarouselItem({
+                    title: post.data.title,
+                    url: post.data.url,
+                    description: `${post.data.score} points, ${post.data.num_comments} comments`,
+                });
+            }),
+        }));
+    })
+    .catch((err) => {
+        conv.ask('Something went wrong! Is that a valid subreddit?');
         console.log(err);
     });
 
